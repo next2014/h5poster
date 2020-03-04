d868fe24cafab2e752940096672e3e95035db8d0
next2014 <83329265@qq.com>
2020-03-04 13:07:35 +0800

    0303

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
-<template>
+<!-- <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
+</template> -->
+<template>
+  <div id="main">
+    <router-view/>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+// export default {
+//   name: 'App',
+//   components: {
+//     HelloWorld
+//   }
+// }
 </script>
 
 <style>
-#app {
+@import "assets/css/reset.css";
+html, body {
+  width: 100%;
+  height: 100%;
+  background-color: #222222;
+}
+#main {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
